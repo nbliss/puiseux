fbf5c5c85b511f70c0fd93b2ba16a2374bd53dc6
nbliss <nbliss2@uic.edu>
2014-09-22 10:46:50 -0500

    Convenience class for ease of use

---
+++ sympyWrapped.py
@@ -0,0 +1,23 @@
+from sympy import poly,pprint
+from sympy.abc import x,y
+from puiseuxPoly import puiseux
+from mypoly import mypoly
+from expandParallel import solutionList
+from fractions import Fraction as fr
+import sys
+"""
+A convenience class that uses Sympy to allow easier command line input.
+Asks user for a polynomial and for the number of desired terms. Poly can
+be input as, for example, (x^3-y)*(2x+4). Sympy takes care of the simplification and the script transforms it into a mypoly object and calls the solutionList function.
+"""
+s = raw_input("Enter a polynomial in x and y --> ") 
+n = input("Enter the number of desired terms --> ")
+s = s.replace('^','**')
+p = poly(eval(s),x,y,domain='CC')
+d = {item[0][1]:puiseux({item[0][0]:item[1]}) for item in p.terms()}
+m = mypoly(d)
+print
+print m
+
+for sol in solutionList(m,n,True):
+    print sol
